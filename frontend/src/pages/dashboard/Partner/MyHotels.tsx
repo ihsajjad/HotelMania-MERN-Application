b5966b380dec316cab59ecd416aead0ab2930348
Iftekher Hossen Sajjad <iftekherhossensajjad@gmail.com>
2024-04-02 19:35:40 +0600

    Frontend: my hotels in my hotels page

--- frontend/src/pages/dashboard/Partner/MyHotels.tsx
+++ frontend/src/pages/dashboard/Partner/MyHotels.tsx
@@ -1,4 +1,7 @@
+import { BiHotel, BiMoney, BiStar } from "react-icons/bi";
+import { BsBuilding, BsMap } from "react-icons/bs";
 import { useQuery } from "react-query";
+import { Link } from "react-router-dom";
 import * as apiClient from "../../../api-client";
 import PageTitle from "../../../components/PageTitle";
 import { useAppContext } from "../../../contexts/UseContexts";
@@ -21,8 +24,45 @@ const MyHotels = () => {
             hotels.map((hotel) => (
               <div
                 key={hotel._id}
-                className="flex gap-4 md:flex-row flex-col w-full p-4 rounded md:h-80 border border-zinc-300"
-              ></div>
+                className="flex gap-4 flex-col w-full p-4 rounded border border-zinc-300"
+              >
+                <h2 className="text-2xl font-bold">{hotel.name}</h2>
+                <div className="whitespace-pre-line line-clamp-3">
+                  {hotel.description}
+                </div>
+                <div className="grid xl:grid-cols-[2fr_1fr_2fr_2fr_2fr] md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-2">
+                  <div className="border text-sm border-slate-300 rounded-sm p-3 flex items-center gap-1">
+                    <BsMap />
+                    {hotel.city}, {hotel.country}
+                  </div>
+                  <div className="border text-sm border-slate-300 rounded-sm p-3 flex items-center gap-1">
+                    <BsBuilding />
+                    {hotel.type}
+                  </div>
+                  <div className="border text-sm border-slate-300 rounded-sm p-3 flex items-center gap-1">
+                    <BiMoney />${hotel.pricePerNight} per night
+                  </div>
+                  <div className="border text-sm border-slate-300 rounded-sm p-3 flex items-center gap-1">
+                    <BiHotel />
+                    {hotel.adultCount} adults, {hotel.childCount} children
+                  </div>
+                  <div className="border text-sm border-slate-300 rounded-sm p-3 flex items-center gap-1">
+                    <BiStar />
+                    {hotel.starRating} Star Rating
+                  </div>
+                </div>
+                <span className="flex gap-3 justify-center sm:justify-end">
+                  <button className="bg-red-500 text-white py-1.5 px-3 rounded font-bold">
+                    Delete
+                  </button>
+                  <button className="bg-orange-500 text-white py-1.5 px-3 rounded font-bold">
+                    Edit
+                  </button>
+                  <Link to={`/details/${hotel._id}`} className="custom-btn">
+                    View Details
+                  </Link>
+                </span>
+              </div>
             ))}
         </div>
       </div>
