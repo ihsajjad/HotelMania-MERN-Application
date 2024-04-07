6abd1797a94c239807570639f1183dc5e5cbf137
Iftekher Hossen Sajjad <iftekherhossensajjad@gmail.com>
2024-04-08 03:38:36 +0600

    Frontend: added pending animation to the partnerRoute

--- frontend/src/routes/PartnerRoute.tsx
+++ frontend/src/routes/PartnerRoute.tsx
@@ -1,11 +1,13 @@
+import Lottie from "lottie-react";
 import { ReactNode } from "react";
 import { FaRegSadTear } from "react-icons/fa";
+import pending from "../assets/pending.json";
 import { useAppContext } from "../contexts/UseContexts";
 
 const PartnerRoute = ({ children }: { children: ReactNode }) => {
   const { user } = useAppContext();
 
-  if (user.role === "Hotel") {
+  if (user.role !== "Hotel") {
     return (
       <div className="flex flex-col items-center justify-center gap-3 h-[80vh] ">
         <FaRegSadTear size={80} color="var(--main-color)" />
@@ -20,7 +22,22 @@ const PartnerRoute = ({ children }: { children: ReactNode }) => {
     return children;
   }
 
-  return <div>NoT verified</div>;
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-center gap-3 h-[80vh] ">
+        <Lottie animationData={pending} className="h-[40vh] w-full" />
+        <h2 className="text-4xl font-semibold text-center text-slate-700 -mt-16">
+          Your account is <br />
+          <span className="text-[var(--main-color)] font-bold text-5xl">
+            Pending
+          </span>
+        </h2>
+        <h2 className="text-lg font-bold text-center text-slate-500 ">
+          Admin will approve your account within 3 business days.
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default PartnerRoute;
