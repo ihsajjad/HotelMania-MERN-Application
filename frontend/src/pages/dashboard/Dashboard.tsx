2edb88dea7153ccf6495e840d40cf429d8b7af01
Iftekher Hossen Sajjad <iftekherhossensajjad@gmail.com>
2024-05-07 21:53:41 +0600

    Frontend: optimized the code for better performance

--- frontend/src/pages/dashboard/Dashboard.tsx
+++ frontend/src/pages/dashboard/Dashboard.tsx
@@ -1,18 +1,23 @@
+import React, { Suspense } from "react";
 import { useAppContext } from "../../contexts/UseContexts";
-import AdminDashboard from "./admin/AdminDashboard";
-import PartnerDashboard from "./partner/PartnerDashboard";
-import UserDashboard from "./user/UserDashboard";
+
+const AdminDashboard = React.lazy(() => import("./admin/AdminDashboard"));
+const PartnerDashboard = React.lazy(() => import("./partner/PartnerDashboard"));
+const UserDashboard = React.lazy(() => import("./user/UserDashboard"));
 
 const Dashboard = () => {
   const { user } = useAppContext();
 
-  if (user.role === "User") {
-    return <UserDashboard />;
-  }
-  if (user.role === "Admin") return <AdminDashboard />;
-
-  if (user.role === "Hotel") return <PartnerDashboard />;
-  return <div>none</div>;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      {user.role === "User" && <UserDashboard />}
+      {user.role === "Admin" && <AdminDashboard />}
+      {user.role === "Hotel" && <PartnerDashboard />}
+      {user.role !== "User" &&
+        user.role !== "Admin" &&
+        user.role !== "Hotel" && <div>None</div>}
+    </Suspense>
+  );
 };
 
 export default Dashboard;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,42 +12,65 @@ export default defineConfig({
             /\/src\/layouts\/DashboardLayout\.tsx$/.test(id) &&
             !/node_modules/.test(id)
           ) {
-            return "dashboard"; // Separate dashboard components into their own chunk
+            return "dashboard";
           } else if (
             /\/src\/pages\/Home\.tsx$/.test(id) &&
             !/node_modules/.test(id)
           ) {
-            return "home"; // Separate dashboard components into their own chunk
+            return "home";
+          } else if (
+            /\/src\/pages\/FindHotels\.tsx$/.test(id) &&
+            !/node_modules/.test(id)
+          ) {
+            return "search";
           } else if (
             /\/src\/pages\/dashboard\/admin\/AdminDashboard\.tsx$/.test(id) &&
             !/node_modules/.test(id)
           ) {
-            return "adminDashboard"; // Separate dashboard components into their own chunk
+            return "adminDashboard";
           } else if (
             /\/src\/pages\/HotelDetails\.tsx$/.test(id) &&
             !/node_modules/.test(id)
           ) {
-            return "hotel_details"; // Separate dashboard components into their own chunk
+            return "hotel_details";
           } else if (
             /\/src\/components\/AddHotelModal\.tsx$/.test(id) &&
             !/node_modules/.test(id)
           ) {
-            return "add_hotel_modal"; // Separate dashboard components into their own chunk
+            return "add_hotel_modal";
           } else if (
             /\/src\/pages\/dashboard\/admin\/AllBookings\.tsx$/.test(id) &&
             !/node_modules/.test(id)
           ) {
-            return "allBookings"; // Separate dashboard components into their own chunk
+            return "allBookings";
+          } else if (
+            /\/src\/components\/.*\.tsx$/.test(id) &&
+            !/node_modules/.test(id)
+          ) {
+            console.log("Id from ", id);
+            return "components-" + id.split("/").pop(); // Separate components into their own chunk
+          } else if (
+            /\/src\/api-client\.ts$/.test(id) &&
+            !/node_modules/.test(id)
+          ) {
+            return "apiClient";
           } else if (
             /\/src\/pages\/dashboard\/partner\/MyHotels\.tsx$/.test(id) &&
             !/node_modules/.test(id)
           ) {
-            return "myHotels"; // Separate dashboard components into their own chunk
+            return "myHotels";
           } else if (
             /\/src\/pages\/dashboard\/admin\/Partners\.tsx$/.test(id) &&
             !/node_modules/.test(id)
           ) {
-            return "partners"; // Separate dashboard components into their own chunk
+            return "partners";
+          } else if (
+            /\/src\/pages\/Login\.tsx$/.test(id) ||
+            /\/src\/pages\/PartnerRegister\.tsx$/.test(id) ||
+            (/\/src\/pages\/Register\.tsx$/.test(id) &&
+              !/node_modules/.test(id))
+          ) {
+            return "register";
           }
         },
       },
